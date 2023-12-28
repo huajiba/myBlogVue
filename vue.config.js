85bcabe8c143d8c92565e4fdff8726ed1c42bb62
La02 <la0shu5@qq.com>
2023-12-28 22:24:38 +0800

    01-setup

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,8 @@ const name = defaultSettings.title || 'vue Admin Template' // page title
 // For example, Mac: sudo npm run
 // You can change the port by the following methods:
 // port = 9528 npm run dev OR npm run dev --port = 9528
-const port = process.env.port || process.env.npm_config_port || 9528 // dev port
+// 设置端口
+const port = 8888 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
@@ -27,11 +28,13 @@ module.exports = {
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
-  lintOnSave: process.env.NODE_ENV === 'development',
+  // 语法校验
+  lintOnSave: false,
   productionSourceMap: false,
   devServer: {
     port: port,
-    open: true,
+    // 自动开启浏览器
+    open: false,
     overlay: {
       warnings: false,
       errors: true
